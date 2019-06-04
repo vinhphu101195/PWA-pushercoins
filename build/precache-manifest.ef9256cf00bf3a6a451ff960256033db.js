e6c0b82b19e83a5134f809230ba06c52581adb97
PhuChau <vinhphu101195@gmail.com>
2019-06-04 18:48:41 +0300

    new change

---
+++ build/precache-manifest.ef9256cf00bf3a6a451ff960256033db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b363a4b5ec2e58cc3fb956b8eccca7",
+    "url": "/vinhphu101195/PWA-pushercoins/index.html"
+  },
+  {
+    "revision": "4fe8bc1ce965083365e0",
+    "url": "/vinhphu101195/PWA-pushercoins/static/css/main.2b8eb05c.chunk.css"
+  },
+  {
+    "revision": "cf4a95594577135e1f71",
+    "url": "/vinhphu101195/PWA-pushercoins/static/js/2.c199dfae.chunk.js"
+  },
+  {
+    "revision": "4fe8bc1ce965083365e0",
+    "url": "/vinhphu101195/PWA-pushercoins/static/js/main.248e0d85.chunk.js"
+  },
+  {
+    "revision": "ae38e4c653d8069770f7",
+    "url": "/vinhphu101195/PWA-pushercoins/static/js/runtime~main.b98781e4.js"
+  }
+]);
